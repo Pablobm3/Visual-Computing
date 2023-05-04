5a039b32154d9b18e77fe542bb2d431826d8e6f8
Ramon <ramongarciatomas@gmail.com>
2023-05-04 13:59:06 +0200

    Practica 1

---
+++ Practice1/OpenGL_Praktikum/cmake-build-debug/libs/glm-master/test/core/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Ramon Garcia/Documents/OpenGL_Praktikum/libs/glm-master/test/core
+# Build directory: C:/Users/Ramon Garcia/Documents/OpenGL_Praktikum/cmake-build-debug/libs/glm-master/test/core
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
